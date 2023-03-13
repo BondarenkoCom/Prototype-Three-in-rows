39b10dc47fe76071d652a7c3c5834507cb75982b
Уденков Виталий <VUdenkov@beeline.ru>
2023-03-13 12:02:09 +0600

    Board manager added.

---
+++ Assets/Scripts/Board and Grid/BoardManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public static BoardManager instance { set; get; }
+    public List<Sprite> characters = new List<Sprite>();
+    public GameObject tile;
+    public int xSize, ySize;
+
+    private GameObject[,] _tiles;
+
+    public bool IsShifting { set; get; }
+    void Start()
+    {
+        instance = GetComponent<BoardManager>();
+
+        Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
+        CreateBoard(offset.x, offset.y);
+    }
+
+    private void CreateBoard(float offsetX, float offsetY)
+    {
+        _tiles = new GameObject[xSize, ySize];
+
+        float startX = transform.position.x;
+        float startY = transform.position.y;
+
+        for (int i = 0; i < xSize; i++)
+        {
+            for (int j = 0; j < ySize; j++)
+            {
+                GameObject newTile = Instantiate(
+                    tile,
+                    new Vector3(startX + (offsetX * i), startY + (offsetY * j), 0),
+                    tile.transform.rotation);
+
+                _tiles[i, j] = newTile;
+            }
+        }
+    }
+}
